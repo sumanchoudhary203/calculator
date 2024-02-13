6469f5efbf8f9af2760af574654e0e0abba09d36
sumanchoudhary203 <sc3868181@gmail.com>
2024-02-13 14:09:46 +0530

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,9 @@
 import { useEffect, useState } from "react";
-import { FaSignal } from "react-icons/fa6";
-import { IoBatteryHalf } from "react-icons/io5";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import { Tab } from "./tabs";
-import { eval1 } from "./utils";
 
 function App() {
-  const [value, setValue] = useState(" ");
-  const [output, setOutput] = useState(" ");
-  const [disabled, setDisabled] = useState(true);
-
-  useEffect(() => {
-    // if (
-    //   value[value.length - 1] == "x" ||
-    //   value[value.length - 1] == "+" ||
-    //   value[value.length - 1] == "-" ||
-    //   value[value.length - 1] == "%" ||
-    //   value[value.length - 1] == "÷" ||
-    //   value[value.length - 1] == "()"
-    // ) {
-    //   setDisabled(true);
-    // } else {
-    //   setDisabled(false);
-    // }
-    try {
-      let a: string = value.replaceAll("x", "*");
-      a = a.replaceAll("÷", "/");
-      setOutput(eval1(a));
-    } catch (error) {}
-  }, [value]);
-
   return (
-    <div className="flex justify-center  items-center  w-full  bg-blue-300">
-      <div className="h-full bg-white w-[350px]  border-blue-300  border-8">
-        <div className="h-8 flex justify-between  p-1 px-3 bg-gray-200">
-          <div>
-            <div>4:00 PM</div>
-          </div>
-          <div className="flex space-x-1">
-            <FaSignal size={19} />
-            <IoBatteryHalf size={21} />
-          </div>
-        </div>
-        <div className="h-9 flex justify-end p-3 bg-gray-200">
-          <BsThreeDotsVertical />
-        </div>
-        <div className="h-48 bg-gray-200   rounded-b-3xl">
-          <input
-            value={value}
-            className="h-12  bg-gray-200  w-full text-[30px] text-end flex justify-end p-12 "
-          />
-          <div className="h-[95px] rounded-b-3xl p-12 flex text-[28px]  justify-end items-end">
-            {output}
-          </div>
-        </div>
-        <Tab
-          value={value}
-          // disabled={disabled}
-          output={output}
-          setValue={(val) => setValue(val)}
-          setOutput={(val) => setOutput(val)}
-        />
-      </div>
+    <div className=" h-screen w-full bg-gray-400  flex justify-center items-center ">
+      <div className="text-black">ghhg</div>
     </div>
   );
 }
-
-export default App;
